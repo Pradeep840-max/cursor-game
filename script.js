cb4fd3324a8c084340fbc180b196cabb6b10ca0f
Pradeep840-max <pradeephm681@gmail.com>
2026-03-02 13:38:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,77 +4,55 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particles = [];
-let mouse = { x: null, y: null };
+let mouse = {
+  x: canvas.width / 2,
+  y: canvas.height / 2
+};
 
-window.addEventListener("mousemove", function (event) {
-  mouse.x = event.x;
-  mouse.y = event.y;
+window.addEventListener("mousemove", function(e) {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
 });
 
-class Particle {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = 2;
-    this.speedX = (Math.random() - 0.5) * 2;
-    this.speedY = (Math.random() - 0.5) * 2;
-  }
-
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-
-    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
-    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
-  }
-
-  draw() {
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
+class Segment {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
 
-function init() {
-  particles = [];
-  for (let i = 0; i < 120; i++) {
-    particles.push(new Particle());
-  }
-}
+let dragon = [];
+let length = 40; // dragon body length
 
-function connect() {
-  for (let a = 0; a < particles.length; a++) {
-    for (let b = a; b < particles.length; b++) {
-      let dx = particles[a].x - particles[b].x;
-      let dy = particles[a].y - particles[b].y;
-      let distance = dx * dx + dy * dy;
-
-      if (distance < 10000) {
-        ctx.strokeStyle = "rgba(255,255,255,0.1)";
-        ctx.lineWidth = 1;
-        ctx.beginPath();
-        ctx.moveTo(particles[a].x, particles[a].y);
-        ctx.lineTo(particles[b].x, particles[b].y);
-        ctx.stroke();
-      }
-    }
-  }
+// Create dragon body
+for (let i = 0; i < length; i++) {
+  dragon.push(new Segment(mouse.x, mouse.y));
 }
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0,0,0,0.3)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  for (let particle of particles) {
-    particle.update();
-    particle.draw();
+  // Head follows mouse
+  dragon[0].x += (mouse.x - dragon[0].x) * 0.2;
+  dragon[0].y += (mouse.y - dragon[0].y) * 0.2;
+
+  // Body follows previous segment
+  for (let i = 1; i < dragon.length; i++) {
+    dragon[i].x += (dragon[i - 1].x - dragon[i].x) * 0.3;
+    dragon[i].y += (dragon[i - 1].y - dragon[i].y) * 0.3;
   }
 
-  connect();
+  // Draw dragon
+  for (let i = dragon.length - 1; i >= 0; i--) {
+    ctx.beginPath();
+    ctx.arc(dragon[i].x, dragon[i].y, 8 - i * 0.1, 0, Math.PI * 2);
+    ctx.fillStyle = hsl(${i * 10}, 100%, 50%);
+    ctx.fill();
+  }
+
   requestAnimationFrame(animate);
 }
 
-init();
 animate();
 
