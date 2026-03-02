0f0f85093731d3d39a5dfb530035567565d3bf44
Pradeep840-max <pradeephm681@gmail.com>
2026-03-02 13:19:42 +0530

    Add particle animation to game canvas

--- script.js
+++ script.js
@@ -1,22 +1,80 @@
-let score = 0;
-let time = 30;
-let timer;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-function startGame() {
-    score = 0;
-    time = 30;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    document.getElementById("score").innerText = score;
-    document.getElementById("time").innerText = time;
+let particles = [];
+let mouse = { x: null, y: null };
 
-    timer = setInterval(() => {
-        time--;
-        document.getElementById("time").innerText = time;
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
 
-        if (time <= 0) {
-            clearInterval(timer);
-            alert("Game Over! Your Score: " + score);
-        }
-    }, 1000);
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = 2;
+    this.speedX = (Math.random() - 0.5) * 2;
+    this.speedY = (Math.random() - 0.5) * 2;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
+
+function init() {
+  particles = [];
+  for (let i = 0; i < 120; i++) {
+    particles.push(new Particle());
+  }
+}
+
+function connect() {
+  for (let a = 0; a < particles.length; a++) {
+    for (let b = a; b < particles.length; b++) {
+      let dx = particles[a].x - particles[b].x;
+      let dy = particles[a].y - particles[b].y;
+      let distance = dx * dx + dy * dy;
+
+      if (distance < 10000) {
+        ctx.strokeStyle = "rgba(255,255,255,0.1)";
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(particles[a].x, particles[a].y);
+        ctx.lineTo(particles[b].x, particles[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let particle of particles) {
+    particle.update();
+    particle.draw();
+  }
+
+  connect();
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
 
